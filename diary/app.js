148446ee8096690db64e16b80b309f256879d432
Vishmi <vishmihgangodawila@gmail.com>
2026-03-28 01:09:09 +0530

    Add hello-world, remove seventh-grade, update app.js
    Update diary/app.js to write a new hello-world.txt and delete the old seventh-grade.txt: adds fs.writeFile to create 'hello-world.txt' with 'Hello World' and fs.unlink to remove 'seventh-grade.txt'. Also commit adds diary/hello-world.txt and deletes diary/seventh-grade.txt to reflect these file operations and demonstrate basic fs read/write/unlink usage.

--- diary/app.js
+++ diary/app.js
@@ -8,3 +8,20 @@ fs.readFile('seventh-grade.txt', 'utf8', (err, data) => {
   }
   console.log(data);
 });
+
+const message = 'Hello World';
+fs.writeFile('hello-world.txt', message, (err) => {
+  if (err) {
+    console.error('Error writing to file:', err);
+  } else {
+    console.log('File written successfully!');
+  }
+});
+
+fs.unlink('seventh-grade.txt',(err) => {
+  if (err) {
+    console.error('Error deleting file:', err);
+    return;
+  }
+  console.log('seventh-grade.txt has been deleted.');
+});
